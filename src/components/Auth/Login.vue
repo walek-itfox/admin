a6f5b19ccdb807d415640788dd426d1fc90e1752
walek-itfox <valentin.s@itfox-web.com>
2019-09-21 17:59:14 +0300

    Add Login component

---
+++ src/components/Auth/Login.vue
@@ -0,0 +1,93 @@
+<template>
+  <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col
+            cols="12"
+            sm="8"
+            md="4"
+          >
+            <v-card class="elevation-12">
+              <v-toolbar
+                color="primary"
+                dark
+                flat
+              >
+                <v-toolbar-title>Login form</v-toolbar-title>
+                
+              </v-toolbar>
+              <v-form v-model="valid" ref="form" validation>
+              <v-card-text>
+                
+                  <v-text-field
+                    label="Email"
+                    name="email"
+                    prepend-icon="person"
+                    type="email"
+                    v-model="email"
+                    :rules="emailRules"
+                  ></v-text-field>
+
+                  <v-text-field
+                    id="password"
+                    label="Password"
+                    name="password"
+                    prepend-icon="lock"
+                    type="password"
+                    v-model="password"
+                    :rules="passwordRules"
+                  ></v-text-field>
+                
+              </v-card-text>
+              <v-card-actions>
+                <div class="flex-grow-1"></div>
+                <v-btn 
+                  color="primary"
+                  @click="onSubmit"
+                  :disabled="!valid"
+                >
+                  Login
+                </v-btn>
+              </v-card-actions>
+              </v-form>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      email: '',
+      password: '',
+      valid: false,
+      passwordRules: [
+        v => !!v || 'Password is required',
+      ],
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+/.test(v) || 'E-mail must be valid',
+      ]
+    }
+  },
+  methods: {
+    onSubmit () {
+      if(this.$refs.form.validate()) {
+        const user = {
+          email: this.email,
+          password: this.password
+        }
+        console.log(user)
+      }
+    }
+  }
+}
+
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,40 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Home from '@/components/Home'
+import Login from '@/components/Auth/Login'
+import Register from '@/components/Auth/Register'
+import Create from '@/components/User/Create'
+import Users from '@/components/User/Users'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-
-  ]
+    {
+      path: "/",
+      name: "Home",
+      component: Home
+    },
+    {
+      path: "/login",
+      name: "Login",
+      component: Login
+    },
+    {
+      path: "/register",
+      name: "Register",
+      component: Register
+    },
+    {
+      path: "/users",
+      name: "Users",
+      component: Users
+    },
+    {
+      path: "/create",
+      name: "Create",
+      component: Create
+    }
+  ],
+  mode: 'history'
 })

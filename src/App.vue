05ddb94dba5a0358ced3e14dc7d10b15b009cb43
walek-itfox <valentin.s@itfox-web.com>
2019-09-21 15:05:22 +0300

    Edit App component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,50 @@
 <template>
 <v-app>
-  <v-navigation-drawer app temporary>
+  <v-navigation-drawer 
+    app 
+    temporary
+    v-model="drawer"
+  >
+    <v-list dense>
+      <v-subheader>MENU</v-subheader>
+      <v-list-item-group color="primary">
+        <v-list-item
+          v-for="item in links"
+          :key="item.title"
+          :to="item.url"
+        >
+          <v-list-item-icon>
+            <v-icon v-text="item.icon"></v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
   </v-navigation-drawer>
   <v-app-bar app dark color="primary">
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-app-bar-nav-icon
+      @click="drawer = !drawer"
+      class="hidden-md-and-up"
+    >
+
+    </v-app-bar-nav-icon>
 
     <v-toolbar-title>Admin panel</v-toolbar-title>
 
     <v-spacer></v-spacer>
-      <v-btn text>
-        <v-icon left>person_add</v-icon>
-        Link 1
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn 
+        text
+        v-for="link in links"
+        :key="link.title"
+        :to="link.url"
+      >
+        <v-icon left>{{link.icon}}</v-icon>
+        {{link.title}}
       </v-btn>
-
-      <v-btn text>
-        <v-icon left>person_add</v-icon>
-        Link 2
-      </v-btn>
-
+    </v-toolbar-items>
   </v-app-bar>
   <v-content>
     <router-view></router-view>
@@ -28,7 +55,28 @@
 <script>
 /* eslint-disable */
 export default {
-  
+  data () {
+    return {
+      drawer: false,
+      links: [
+        {
+          icon: "how_to_reg",
+          title: "Login",
+          url: "/login"
+        },
+        {
+          icon: "people",
+          title: "Users",
+          url: "/users"
+        },
+        {
+          icon: "person_add",
+          title: "New user",
+          url: "/create"
+        }
+      ]
+    }
+  }
 }
 </script>
 
